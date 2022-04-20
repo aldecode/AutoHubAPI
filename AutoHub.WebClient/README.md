f1c364bc2d32debb63f978e2bbd8f58b84b75794
Yuliia <laptievajulia@gmail.com>
2022-04-20 22:32:35 +0300

    Changed web-api to web-client

--- AutoHub.WebClient/README.md
+++ AutoHub.WebClient/README.md
@@ -1,4 +1,4 @@
-# AutoHub.WebApi
+# AutoHub.WebClient
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.3.3.
 
--- AutoHub.WebClient/src/app/app.module.ts
+++ AutoHub.WebClient/src/app/app.module.ts
@@ -3,13 +3,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule,
+    HttpClientModule,
+    SharedModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
